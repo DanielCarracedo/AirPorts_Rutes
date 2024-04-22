df6e29ffdb7312b873e3ad43700e304306ba6a8e
DanielCarracedo <carracedod@uninorte.edu.co>
2024-04-22 13:28:37 -0500

    Ahhh

---
+++ Airports.py
@@ -0,0 +1,19 @@
+class Airport():
+    
+    def __init__(self,name:str, code,city:str, country:str,Latitude,Longitude) -> None:
+        self.__name = name
+        self.__code = code
+        self.__city = city
+        self.__country = country
+        self.__Latitude = Latitude
+        self.__Longitude = Longitude
+    
+    def Airport_Info(self):
+        pass
+    
+    def MostFar_10Airports(self):
+        pass
+    
+    def Airports_min(self):
+        pass
+    
